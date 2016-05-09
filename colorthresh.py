f722d8221a82a0987e1cebe2d57d3e536ce4ebeb
Christopher Schmidt <christopher.schmidt@posteo.de>
2016-05-09 22:31:18 +0200

    Switched to class-based filters and implemented RGBThresholding

--- colorthresh.py
+++ colorthresh.py
@@ -2,18 +2,56 @@ import argparse
 from PIL import Image
 
 
-def hsv_thresh(im, threshold = 200):
-    px = im.load()
+class Filter():
+    def __init__(self):
+        pass
 
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
-            h, s, v = px[i, j]
-            if v > threshold:
-                px[i, j] = (0, 0, 255)
-            else:
-                px[i, j] = (h, 255, v)
+    def filter(self, image, **kwargs):
+        pixel = image.load()
 
-    return im
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                a, b, c = pixel[i, j]
+                pixel[i, j] = self.modify(a, b, c, **kwargs)
+
+        return image
+
+    def modify(self, a, b, c, **kwargs):
+        return a, b, c
+
+
+class HSVThreshold(object, Filter):
+    def filter(self, image, **kwargs):
+        return super(HSVThreshold, self).filter(image.convert("HSV"), **kwargs)
+
+    def modify(self, a, b, c, **kwargs):
+        if c > kwargs["threshold"]:
+            return 0, 0, 255
+        else:
+            return a, 255, c
+
+
+class RGBThreshold(object, Filter):
+    def filter(self, image, **kwargs):
+        return super(RGBThreshold, self).filter(image.convert("RGB"), **kwargs)
+
+    def modify(self, a, b, c, **kwargs):
+        t = kwargs["threshold"]
+
+        # Red
+        if a > t: a = 255
+        else: a = 0
+
+        # Green
+        if b > t: b = 255
+        else: b = 0
+
+        # Blue
+        if c > t: c = 255
+        else: c = 0
+
+        return a, b, c
+
 
 '''
 Argument parsing is handled at this point
@@ -24,7 +62,7 @@ parser.add_argument('--output', type=str, help='Output file')
 args = parser.parse_args()
 
 # Read the image
-im = Image.open(args.input).convert("HSV")
+im = Image.open(args.input)
 
-hsv_thresh(im).show()
+RGBThreshold().filter(im, threshold=130).show()
 
